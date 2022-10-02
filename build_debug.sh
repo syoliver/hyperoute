149f2d868f345e68e0c85cb17cf3ff5449e6c0bc
Sylvain Oliver <50275847+syoliver@users.noreply.github.com>
2022-10-02 21:16:41 +0200

    Add github workflow

--- build_debug.sh
+++ build_debug.sh
@@ -3,7 +3,7 @@
 rm -rf build
 mkdir build
 cd build
-conan install .. -s build_type=Debug
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON
+conan install .. -s build_type=Debug --build=missing
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON -DTEST_OUTPUT=JUNIT
 make
 
--- src/route_trie.cpp
+++ src/route_trie.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <iostream>
 #include <algorithm>
+#include <limits>
 
 namespace hyperoute
 {
